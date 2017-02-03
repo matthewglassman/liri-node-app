c0c9394eebd88a8c1a4251c4d8c9e26aa810c304
matthewglassman <matthewglassmn78@gmail.com>
2017-02-03 15:34:16 -0500

    Created key.js and random.txt

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key:'gfmqnaXQxtWrfdD0YpxTmX5xI',
+	consumer_secret: 'xIVwqymj9pH0ZnBuoZs9siBd6ZTtqldRBWLZd3ZliTqUpMTObR',
+	access_token_key: '331656337-PEvd46nKB7qC1p7M0lhFSsAqOWOx05VsDKbJoy1Y',
+	access_token_secret: 'SbmzLPfne5RKzoPIrz8JZjiueEVj3Yr6HOEY6fvoiIUb0',
+}
