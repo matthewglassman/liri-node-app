56f8390b4e166132dca1cf3e2b2562efa54e3c21
matthewglassman <matthewglassmn78@gmail.com>
2017-02-03 21:02:34 -0500

    Added the requires so far to the main application

---
+++ liri.js
@@ -0,0 +1,6 @@
+// Need a constructor for the keys and twitter?
+var keys = require("./keys.js") 
+var twitter = require("twitter");
+var omdb = require("request");
+var spotify = require("spotify");
+var filesystem = require("fs");
