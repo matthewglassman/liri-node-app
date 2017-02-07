aae1e90bca2d3dbc97382177176e98a0ee8939e8
matthewglassman <matthewglassmn78@gmail.com>
2017-02-06 20:53:15 -0500

    Got twitter keys and object working with file exports

--- liri.js
+++ liri.js
@@ -4,3 +4,15 @@ var twitter = require("twitter");
 var omdb = require("request");
 var spotify = require("spotify");
 var filesystem = require("fs");
+
+var liriCommand = process.argv[2];
+
+var twitterKeys = keys.twitterKeys;
+
+var twitterUser = new twitter({
+	consumer_key: twitterKeys.consumer_key,
+	consumer_secret: twitterKeys.consumer_secret,
+	access_token_key: twitterKeys.access_token_key,
+	access_token_secret: twitterKeys.access_token_secret
+});
+console.log(twitterUser);
