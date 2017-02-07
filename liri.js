f19cfa5b72d857d571777c50b737263c0a04b808
matthewglassman <matthewglassmn78@gmail.com>
2017-02-06 21:28:43 -0500

    Laying out where rest of the code should go.  Organizing notes

--- liri.js
+++ liri.js
@@ -15,4 +15,39 @@ var twitterUser = new twitter({
 	access_token_key: twitterKeys.access_token_key,
 	access_token_secret: twitterKeys.access_token_secret
 });
-console.log(twitterUser);
+//console.log(twitterUser); okay keys working
+
+//Switch cases to call function based on liriCommand entered.
+switch(liriCommand) {
+	case "my-tweets":
+	myTweets();
+	break;
+
+	case "spotify-this-song":
+	myPlayList();
+	break;
+
+	case "movie-this":
+	myMovie();
+	break;
+
+	case "do-what-it-says":
+	randomPick();
+	break;
+}
+
+//Function for pulling in last 20 tweets
+
+function myTweets(){};
+
+//Function for pulling in artist, song name, link to song and album with song via Spotify
+function myPlayList(){};
+
+//Function for using request to get OMDB movie information from movie object
+// Move Title, Year of release, Rating, Country, Language, Plot, Actors, Rotten Tomato Rating, URL
+
+function myMovie(){};
+
+// Function to fire off if typed do what it says
+
+function randomPick(){};
